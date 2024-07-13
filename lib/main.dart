261d94d9739e4f19a75a27e66a91fe57e07f43f4
HazimNabil <HazemNabil95@outlook.com>
2024-07-13 21:11:12 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CheckMate());
+}
+
+class CheckMate extends StatelessWidget {
+  const CheckMate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
