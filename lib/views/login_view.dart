163199e78e32fde0206787e14e8eac32f0c00d91
HazimNabil <HazemNabil95@outlook.com>
2024-07-17 02:07:59 +0200

    Add loading indicator to login and register views

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,8 @@
+import 'package:check_mate/constants.dart';
 import 'package:check_mate/views/register_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import '../helper/show_snack_bar.dart';
 import '../widgets/auth_button.dart';
@@ -18,112 +20,121 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   String? email, password;
   final formKey = GlobalKey<FormState>();
+  bool isLoading = false;
   var autovalidateMode = AutovalidateMode.disabled;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Form(
-        key: formKey,
-        autovalidateMode: autovalidateMode,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Login',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
+    return ModalProgressHUD(
+      progressIndicator: const CircularProgressIndicator(
+        color: kPrimaryColor,
+      ),
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        body: Form(
+          key: formKey,
+          autovalidateMode: autovalidateMode,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 130),
-            CustomTextField(
-              hint: 'Email',
-              onChanged: (value) {
-                email = value;
-              },
-              validator: (value) {
-                final emailPattern = RegExp(
-                  r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
-                );
-                if (value?.isEmpty ?? true) {
-                  return 'This field is required';
-                } else if (!emailPattern.hasMatch(value!)) {
-                  return 'This is not valid email';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            CustomTextField(
-              hint: 'Password',
-              onChanged: (value) {
-                password = value;
-              },
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return 'This field is required';
-                }
-                return null;
-              },
-              obscureText: true,
-            ),
-            const SizedBox(height: 50),
-            AuthButton(
-              title: 'Login',
-              onPressed: () async {
-                if (formKey.currentState!.validate()) {
-                  try {
-                    await FirebaseAuth.instance.signInWithEmailAndPassword(
-                      email: email!,
-                      password: password!,
-                    );
-                    showSnackBar(
-                      context,
-                      'Login successful! Welcome back!',
-                    );
-                  } on FirebaseAuthException catch (e) {
-                    if (e.code == 'invalid-credential') {
+              const SizedBox(height: 130),
+              CustomTextField(
+                hint: 'Email',
+                onChanged: (value) {
+                  email = value;
+                },
+                validator: (value) {
+                  final emailPattern = RegExp(
+                    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+                  );
+                  if (value?.isEmpty ?? true) {
+                    return 'This field is required';
+                  } else if (!emailPattern.hasMatch(value!)) {
+                    return 'This is not valid email';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              CustomTextField(
+                hint: 'Password',
+                onChanged: (value) {
+                  password = value;
+                },
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'This field is required';
+                  }
+                  return null;
+                },
+                obscureText: true,
+              ),
+              const SizedBox(height: 50),
+              AuthButton(
+                title: 'Login',
+                onPressed: () async {
+                  if (formKey.currentState!.validate()) {
+                    setState(() => isLoading = true);
+                    try {
+                      await FirebaseAuth.instance.signInWithEmailAndPassword(
+                        email: email!,
+                        password: password!,
+                      );
                       showSnackBar(
                         context,
-                        'Invalid credentials. Please double-check your email and password, and try again',
+                        'Login successful! Welcome back!',
                       );
-                    } else {
-                      showSnackBar(context, e.code);
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'invalid-credential') {
+                        showSnackBar(
+                          context,
+                          'Invalid credentials. Please double-check your email and password, and try again',
+                        );
+                      } else {
+                        showSnackBar(context, e.code);
+                      }
+                    } catch (e) {
+                      showSnackBar(context, e.toString());
                     }
-                  } catch (e) {
-                    showSnackBar(context, e.toString());
+                    setState(() => isLoading = false);
+                  } else {
+                    setState(() => autovalidateMode = AutovalidateMode.always);
                   }
-                } else {
-                  setState(() => autovalidateMode = AutovalidateMode.always);
-                }
-              },
-            ),
-            const SizedBox(height: 15),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Don't have account? ",
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, RegisterView.route);
-                  },
-                  child: const Text(
-                    'Register here',
+                },
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have account? ",
                     style: TextStyle(
                       fontSize: 16,
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              ],
-            )
-          ],
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, RegisterView.route);
+                    },
+                    child: const Text(
+                      'Register here',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,9 @@
 import 'package:check_mate/views/login_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
+import '../constants.dart';
 import '../helper/show_snack_bar.dart';
 import '../widgets/auth_button.dart';
 import '../widgets/custom_text_field.dart';
@@ -18,128 +20,137 @@ class RegisterView extends StatefulWidget {
 class _RegisterViewState extends State<RegisterView> {
   String? email, password, confirmedPassword;
   final formKey = GlobalKey<FormState>();
+  bool isLoading = false;
   var autovalidateMode = AutovalidateMode.disabled;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Form(
-        key: formKey,
-        autovalidateMode: autovalidateMode,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Register',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 130),
-            CustomTextField(
-              hint: 'Email',
-              onChanged: (value) {
-                email = value;
-              },
-              validator: (value) {
-                final emailPattern = RegExp(
-                  r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
-                );
-                if (value?.isEmpty ?? true) {
-                  return 'This field is required';
-                } else if (!emailPattern.hasMatch(value!)) {
-                  return 'This is not valid email';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            CustomTextField(
-              hint: 'Password',
-              onChanged: (value) {
-                password = value;
-              },
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return 'This field is required';
-                }
-                return null;
-              },
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            CustomTextField(
-              hint: 'Confirm Password',
-              onChanged: (value) {
-                confirmedPassword = value;
-              },
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return 'This field is required';
-                } else if (confirmedPassword != password) {
-                  return 'Passwords do not match';
-                }
-                return null;
-              },
-              obscureText: true,
-            ),
-            const SizedBox(height: 50),
-            AuthButton(
-              title: 'Register',
-              onPressed: () async {
-                if (formKey.currentState!.validate()) {
-                  try {
-                    await register();
-                    showSnackBar(
-                      context,
-                      'The registration is done successfully',
-                    );
-                  } on FirebaseAuthException catch (e) {
-                    if (e.code == 'weak-password') {
-                      showSnackBar(
-                        context,
-                        'The provided password is too weak',
-                      );
-                    } else if (e.code == 'email-already-in-use') {
-                      showSnackBar(
-                        context,
-                        'The provided email is already used',
-                      );
-                    }
-                  } catch (e) {
-                    showSnackBar(context, e.toString());
-                  }
-                } else {
-                  setState(() => autovalidateMode = AutovalidateMode.always);
-                }
-              },
-            ),
-            const SizedBox(height: 15),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Don't have account? ",
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
+    return ModalProgressHUD(
+      progressIndicator: const CircularProgressIndicator(
+        color: kPrimaryColor,
+      ),
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        body: Form(
+          key: formKey,
+          autovalidateMode: autovalidateMode,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Register',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
                 ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, LoginView.route);
-                  },
-                  child: const Text(
-                    'Login here',
+              ),
+              const SizedBox(height: 130),
+              CustomTextField(
+                hint: 'Email',
+                onChanged: (value) {
+                  email = value;
+                },
+                validator: (value) {
+                  final emailPattern = RegExp(
+                    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+                  );
+                  if (value?.isEmpty ?? true) {
+                    return 'This field is required';
+                  } else if (!emailPattern.hasMatch(value!)) {
+                    return 'This is not valid email';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              CustomTextField(
+                hint: 'Password',
+                onChanged: (value) {
+                  password = value;
+                },
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'This field is required';
+                  }
+                  return null;
+                },
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              CustomTextField(
+                hint: 'Confirm Password',
+                onChanged: (value) {
+                  confirmedPassword = value;
+                },
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'This field is required';
+                  } else if (confirmedPassword != password) {
+                    return 'Passwords do not match';
+                  }
+                  return null;
+                },
+                obscureText: true,
+              ),
+              const SizedBox(height: 50),
+              AuthButton(
+                title: 'Register',
+                onPressed: () async {
+                  if (formKey.currentState!.validate()) {
+                    setState(() => isLoading = true);
+                    try {
+                      await register();
+                      showSnackBar(
+                        context,
+                        'The registration is done successfully',
+                      );
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'weak-password') {
+                        showSnackBar(
+                          context,
+                          'The provided password is too weak',
+                        );
+                      } else if (e.code == 'email-already-in-use') {
+                        showSnackBar(
+                          context,
+                          'The provided email is already used',
+                        );
+                      }
+                    } catch (e) {
+                      showSnackBar(context, e.toString());
+                    }
+                    setState(() => isLoading = false);
+                  } else {
+                    setState(() => autovalidateMode = AutovalidateMode.always);
+                  }
+                },
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have account? ",
                     style: TextStyle(
                       fontSize: 16,
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              ],
-            )
-          ],
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, LoginView.route);
+                    },
+                    child: const Text(
+                      'Login here',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
