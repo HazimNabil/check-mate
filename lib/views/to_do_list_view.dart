a25a7ff6d2f5c2f724780c2643c049df2bdf6fd5
HazimNabil <HazemNabil95@outlook.com>
2024-07-18 02:30:04 +0200

    Build to-do list view app bar

--- lib/views/to_do_list_view.dart
+++ lib/views/to_do_list_view.dart
@@ -1,12 +1,46 @@
+import 'package:check_mate/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ToDoListView extends StatelessWidget {
   static String route = 'to_do_list_view';
 
   const ToDoListView({super.key});
 
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      actions: [
+        Container(
+          margin: const EdgeInsets.only(right: 16),
+          padding: const EdgeInsets.fromLTRB(10, 8, 16, 8),
+          decoration: BoxDecoration(
+            color: kBackgroundColor,
+            border: Border.all(color: kPrimaryColor, width: 2),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: const Row(
+            children: [
+              Icon(FontAwesomeIcons.mapPin, size: 12),
+              SizedBox(width: 7),
+              Text(
+                'Pin',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: buildAppBar(),
+    );
   }
 }
