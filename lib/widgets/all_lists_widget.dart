e89010c6f476c43abb47eb92481de7e84ecbfa2c
HazimNabil <HazemNabil95@outlook.com>
2024-07-19 03:46:09 +0200

    Improve layout and spacing in UI

--- lib/widgets/all_lists_widget.dart
+++ lib/widgets/all_lists_widget.dart
@@ -7,21 +7,23 @@ class AllListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 100),
-        Image.asset('assets/images/todo svg.png'),
-        const SizedBox(height: 89),
-        const Text(
-          'Create your first to-do list...',
-          style: TextStyle(
-            fontSize: 23,
-            fontWeight: FontWeight.w600,
+    return Center(
+      child: Column(
+        children: [
+          const SizedBox(height: 170),
+          Image.asset('assets/images/todo svg.png'),
+          const SizedBox(height: 89),
+          const Text(
+            'Create your first to-do list...',
+            style: TextStyle(
+              fontSize: 23,
+              fontWeight: FontWeight.w600,
+            ),
           ),
-        ),
-        const SizedBox(height: 48),
-        const NewListButton(),
-      ],
+          const SizedBox(height: 48),
+          const NewListButton(),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/pinned_lists_widget.dart
+++ lib/widgets/pinned_lists_widget.dart
@@ -7,21 +7,23 @@ class PinnedListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 100),
-        Image.asset('assets/images/Group 42.png'),
-        const SizedBox(height: 89),
-        const Text(
-          'Ooops! No pinned list yet...',
-          style: TextStyle(
-            fontSize: 23,
-            fontWeight: FontWeight.w600,
+    return Center(
+      child: Column(
+        children: [
+          const SizedBox(height: 170),
+          Image.asset('assets/images/Group 42.png'),
+          const SizedBox(height: 89),
+          const Text(
+            'Ooops! No pinned list yet...',
+            style: TextStyle(
+              fontSize: 23,
+              fontWeight: FontWeight.w600,
+            ),
           ),
-        ),
-        const SizedBox(height: 48),
-        const NewListButton(),
-      ],
+          const SizedBox(height: 48),
+          const NewListButton(),
+        ],
+      ),
     );
   }
 }
