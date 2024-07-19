1a67a081da0dc8e0acdd9a201bb1018103d5282f
HazimNabil <HazemNabil95@outlook.com>
2024-07-19 03:38:49 +0200

    Remove cupertino segmented control

--- lib/widgets/home_body.dart
+++ lib/widgets/home_body.dart
@@ -1,73 +1,13 @@
-import 'package:check_mate/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'all_lists_widget.dart';
-import 'pinned_lists_widget.dart';
 
-class HomeBody extends StatefulWidget {
+class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
 
   @override
-  State<HomeBody> createState() => _HomeBodyState();
-}
-
-class _HomeBodyState extends State<HomeBody> {
-  int _selectedSegment = 0;
-  final children = {
-    0: const Padding(
-      padding: EdgeInsets.symmetric(vertical: 12),
-      child: Text(
-        'All Lists',
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    ),
-    1: const Padding(
-      padding: EdgeInsets.symmetric(vertical: 12),
-      child: Text(
-        'Pinned',
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    ),
-  };
-
-  Widget buildSegmentContent(int selectedSegment) {
-    if (_selectedSegment == 0) {
-      return const AllListWidget();
-    } else {
-      return const PinnedListWidget();
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          const SizedBox(height: 20),
-          SizedBox(
-            width: 340,
-            child: CupertinoSegmentedControl<int>(
-              children: children,
-              onValueChanged: (int value) {
-                setState(() => _selectedSegment = value);
-              },
-              groupValue: _selectedSegment,
-              selectedColor: kPrimaryColor,
-              pressedColor: const Color(0xFFE5E5E5),
-              borderColor: Colors.transparent,
-              unselectedColor: const Color(0xFFE5E5E5),
-            ),
-          ),
-          buildSegmentContent(_selectedSegment),
-        ],
-      ),
-    );
+    return const AllListWidget();
   }
 }
