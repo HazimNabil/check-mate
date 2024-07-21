d404cdfc543e8e22d70768df41bc3821390f65f9
HazimNabil <HazemNabil95@outlook.com>
2024-07-21 21:06:52 +0200

    Change to-do list tile layout and content size

--- lib/widgets/to_do_list_tile.dart
+++ lib/widgets/to_do_list_tile.dart
@@ -1,8 +1,4 @@
-import 'package:check_mate/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-import 'list_sub_title.dart';
 
 class ToDoListTile extends StatelessWidget {
   const ToDoListTile({super.key});
@@ -12,26 +8,22 @@ class ToDoListTile extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
       child: ListTile(
-        contentPadding: const EdgeInsets.symmetric(
-          vertical: 16,
-          horizontal: 22,
-        ),
         tileColor: Colors.blue,
-        title: const Padding(
-          padding: EdgeInsets.only(bottom: 24.0),
-          child: Text(
-            "Daily To-do's",
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-            ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 2),
+        horizontalTitleGap: 0,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.check_box_outline_blank,
+            size: 21,
           ),
+          onPressed: () {},
         ),
-        subtitle: const ListSubTitle(),
-        trailing: const Icon(
-          FontAwesomeIcons.trash,
-          color: kPrimaryColor,
-          size: 20,
+        title: const Text(
+          "Write code",
+          style: TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
