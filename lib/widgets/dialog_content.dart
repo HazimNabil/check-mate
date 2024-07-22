9e954a461086ae86e4eee51efacad42c71dbca2f
HazimNabil <HazemNabil95@outlook.com>
2024-07-22 03:16:53 +0200

    Add validation and on changed to dialog text field

--- lib/widgets/dialog_content.dart
+++ lib/widgets/dialog_content.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 import 'custom_text_field.dart';
 
-class DialogContent extends StatelessWidget {
+class DialogContent extends StatefulWidget {
   final labels = const [
     'Personal',
     'Work',
@@ -12,6 +12,15 @@ class DialogContent extends StatelessWidget {
   ];
 
   const DialogContent({super.key});
+
+  @override
+  State<DialogContent> createState() => _DialogContentState();
+}
+
+class _DialogContentState extends State<DialogContent> {
+  final formKey = GlobalKey<FormState>();
+  var autovalidateMode = AutovalidateMode.disabled;
+  String? title;
 
   DropdownMenuItem<String> toDropdownItem(label) {
     return DropdownMenuItem(
@@ -22,35 +31,45 @@ class DialogContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: CustomTextField(
-            hint: 'Task title',
-            onChanged: (value) {},
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: CustomTextField(
+              hint: 'Task title',
+              onChanged: (data) => title = data,
+              validator: (data) {
+                if (data?.isEmpty ?? true) {
+                  return 'This field is required';
+                }
+                return null;
+              },
+            ),
           ),
-        ),
-        const SizedBox(height: 10),
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-          decoration: BoxDecoration(
-            color: const Color(0xFFE3E2E2),
-            borderRadius: BorderRadius.circular(8),
+          const SizedBox(height: 10),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+            decoration: BoxDecoration(
+              color: const Color(0xFFE3E2E2),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: DropdownButton(
+              isExpanded: true,
+              iconEnabledColor: kPrimaryColor,
+              padding: const EdgeInsets.only(left: 16, right: 8),
+              borderRadius: BorderRadius.circular(8),
+              dropdownColor: const Color(0xFFE3E2E2),
+              hint: const Text('Task label'),
+              items: widget.labels.map(toDropdownItem).toList(),
+              onChanged: (value) {},
+            ),
           ),
-          child: DropdownButton(
-            isExpanded: true,
-            iconEnabledColor: kPrimaryColor,
-            padding: const EdgeInsets.only(left: 16, right: 8),
-            borderRadius: BorderRadius.circular(8),
-            dropdownColor: const Color(0xFFE3E2E2),
-            hint: const Text('Task label'),
-            items: labels.map(toDropdownItem).toList(),
-            onChanged: (value) {},
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
