7b0ba15811f16517739f7e4031477329f268d6e1
HazimNabil <HazemNabil95@outlook.com>
2024-07-23 03:30:55 +0200

    Solve keyboard issue in all tasks and pinned tasks bodies

--- lib/widgets/all_tasks_empty.dart
+++ lib/widgets/all_tasks_empty.dart
@@ -6,20 +6,21 @@ class AllTasksEmpty extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        children: [
-          const SizedBox(height: 170),
-          Image.asset('assets/images/todo svg.png'),
-          const SizedBox(height: 89),
-          const Text(
-            'Create your first to-do list...',
-            style: TextStyle(
-              fontSize: 23,
-              fontWeight: FontWeight.w600,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('assets/images/todo svg.png'),
+            const SizedBox(height: 89),
+            const Text(
+              'Create your first to-do list...',
+              style: TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ),
-          const SizedBox(height: 48),
-        ],
+            const SizedBox(height: 48),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/pinned_lists_widget.dart
+++ lib/widgets/pinned_lists_widget.dart
@@ -6,20 +6,21 @@ class PinnedListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        children: [
-          const SizedBox(height: 170),
-          Image.asset('assets/images/Group 42.png'),
-          const SizedBox(height: 89),
-          const Text(
-            'Ooops! No pinned list yet...',
-            style: TextStyle(
-              fontSize: 23,
-              fontWeight: FontWeight.w600,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('assets/images/Group 42.png'),
+            const SizedBox(height: 89),
+            const Text(
+              'Ooops! No pinned list yet...',
+              style: TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ),
-          const SizedBox(height: 48),
-        ],
+            const SizedBox(height: 48),
+          ],
+        ),
       ),
     );
   }
