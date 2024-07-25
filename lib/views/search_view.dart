a7256437cde76ae8ec4b9b06e69daedbd7615fe5
HazimNabil <HazemNabil95@outlook.com>
2024-07-25 23:22:10 +0200

    Add search task feature

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,13 +1,27 @@
+import 'package:check_mate/constants.dart';
 import 'package:check_mate/widgets/custom_text_field.dart';
+import 'package:check_mate/widgets/loading_indicator.dart';
+import 'package:check_mate/widgets/something_went_wrong.dart';
+import 'package:check_mate/widgets/task_list.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../widgets/cancel_button.dart';
 
-class SearchView extends StatelessWidget {
+class SearchView extends StatefulWidget {
   static String route = 'search_view';
 
   const SearchView({super.key});
+
+  @override
+  State<SearchView> createState() => _SearchViewState();
+}
+
+class _SearchViewState extends State<SearchView> {
+  List<QueryDocumentSnapshot<Map<String, dynamic>>> tasks = [];
+  Stream<QuerySnapshot<Map<String, dynamic>>>? stream;
+  String searchTerm = '';
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +33,14 @@ class SearchView extends StatelessWidget {
         titleSpacing: 0,
         title: CustomTextField(
           hint: 'Search your list',
-          onChanged: (value) {},
+          onChanged: (value) {
+            setState(() {
+              searchTerm = value.toLowerCase();
+              var taskCollection =
+                  FirebaseFirestore.instance.collection(kTaskCollection);
+              stream = taskCollection.snapshots();
+            });
+          },
           icon: const Icon(
             FontAwesomeIcons.magnifyingGlass,
             size: 18,
@@ -28,6 +49,24 @@ class SearchView extends StatelessWidget {
         actions: const [
           CancelButton(),
         ],
+      ),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: stream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingIndicator();
+          } else if (snapshot.hasData) {
+            tasks = snapshot.data!.docs.where((doc) {
+              var title = (doc['title'] as String).toLowerCase();
+              return title.contains(searchTerm);
+            }).toList();
+            return TaskList(tasks: tasks);
+          } else if (snapshot.hasError) {
+            return SomethingWentWrong(message: snapshot.error.toString());
+          } else {
+            return const TaskList(tasks: []);
+          }
+        },
       ),
     );
   }
