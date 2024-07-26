b4a881cc11ef61e441046dd77aa0b50b838795ee
HazimNabil <HazemNabil95@outlook.com>
2024-07-26 22:57:00 +0200

    Add add_task method to task_service class

--- lib/services/task_service.dart
+++ lib/services/task_service.dart
@@ -1,7 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../constants.dart';
+import '../helper/get_label_color.dart';
+import '../models/task_model.dart';
 
 class TaskService {
   var taskCollection = FirebaseFirestore.instance.collection(kTaskCollection);
+
+  Future<void> addTask(Task task) async {
+    var userId = FirebaseAuth.instance.currentUser!.uid;
+    try {
+      await taskCollection.add({
+        'id': userId,
+        'title': task.title,
+        'isChecked': false,
+        'isPinned': false,
+        'label': task.label,
+        'color': getLabelColor(task.label),
+      });
+    } catch (e) {
+      throw FormatException(e.toString());
+    }
+  }
 }
